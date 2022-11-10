db4b9b83a716d367da87b41ecd50e18da15c6e5d
cblawe <67339121+cblawe@users.noreply.github.com>
2022-11-10 16:45:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+     count += 1
+     countEl.textContent = count
+ }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    //reset to 0
+    countEl.textContent = 0
+    count = 0
+}
